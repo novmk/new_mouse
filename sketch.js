649b9f77a07f3894022120c2fa21f7be7dcd51b9
novmk <67250036+novmk@users.noreply.github.com>
2023-03-01 04:54:21 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -42,8 +42,8 @@ function draw() {
 	  noStroke();
 	  fill(0, 0, 0);
       background(255, 255, 255);
-	  ellipse(positions[27][0], positions[27][1] + 20, 44, 60);
-	  ellipse(positions[32][0], positions[32][1] + 20, 44, 60); 
+	  ellipse(positions[27][0], positions[27][1] + 20, 54, 70);
+	  ellipse(positions[32][0], positions[32][1] + 20, 54, 70); 
       
       // add lines on eyebrow
       noFill();
@@ -87,7 +87,7 @@ function draw() {
       let eyeLeftB = createVector(positions[68][0], positions[68][1]);
       let eyeLeftDist = eyeLeftB.y - eyeLeftT.y;
       // console.log(eyeLeftDist);
-      if (eyeLeftDist > 0 && eyeLeftDist < 2.2) {
+      if (eyeLeftDist > 1.5 && eyeLeftDist < 2.2) {
         stroke(0, 255, 0);
         strokeWeight(1);
         noFill();
@@ -98,7 +98,7 @@ function draw() {
       let eyeRightB = createVector(positions[64][0], positions[64][1]);
       let eyeRightDist = eyeRightB.y - eyeRightT.y;
       // console.log(eyeRightDist);
-      if (eyeRightDist > 0 && eyeRightDist < 2.2) {
+      if (eyeRightDist > 1.5 && eyeRightDist < 2.2) {
         stroke(0, 0, 255);
         strokeWeight(1);
         noFill();
